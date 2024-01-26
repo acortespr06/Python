70d1a58bd5ca113256ea71bd730299b6531f5f5b
Andres Cortes <67211732+acortespr06@users.noreply.github.com>
2024-01-26 16:35:03 -0600

    Create anime-rss-v1.py

---
+++ scripts/anime-rss-v1.py
@@ -0,0 +1,63 @@
+import requests
+import feedparser
+import asyncio
+from datetime import datetime
+import guilded_webhook as guilded
+from bs4 import BeautifulSoup
+
+# Define the list of keywords to skip
+skip_keywords = ["(Tamil Dub)", "(Telugu Dub)", "(Hindi Dub)", "(Italian Dub)", "(Castilian Dub)", "(French Dub)", "(German Dub)", "(Spanish Dub)", "(Portuguese Dub)"]
+
+async def post_to_guilded(rss_feed_url, webhook_url):
+    try:
+        # Fetch and parse the RSS feed
+        feed = feedparser.parse(rss_feed_url)
+
+        # Check if the feed has entries
+        if not feed.entries:
+            raise ValueError('No entries found in the RSS feed.')
+
+        # Create a Guilded webhook
+        hook = guilded.AsyncWebhook(webhook_url)
+
+        # Process each entry
+        for entry in feed.entries:
+            # Parse the publication date from the entry
+            pub_date = datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z")
+
+            # Check if the entry is from today
+            if pub_date.date() == datetime.now().date():
+                title = entry.title
+
+                # Check if any of the skip keywords are present in the title
+                if any(keyword in title for keyword in skip_keywords):
+                    print(f'Skipping entry with title: {title}')
+                    continue
+
+                link = entry.link
+                thumbnail_url = entry.media_thumbnail[0]['url'] 
+                description = entry.description if hasattr(entry, 'description') else ''
+
+                # Use BeautifulSoup to remove img and br tags from the description
+                soup = BeautifulSoup(description, 'html.parser')
+                for tag in soup.find_all(['img', 'br']):
+                    tag.decompose()
+
+                # Create a Guilded embed
+                embed = guilded.Embed(title=title, description=f'{str(soup)}\n\n[Read more]({link})', color=0x00ffff, timestamp=datetime.now())
+                embed.set_image(thumbnail_url)
+
+                # Send data to the webhook
+                await hook.send(content='', embeds=embed)
+
+                print(f'Webhook successfully triggered for {title}')
+
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
+
+# Replace with your actual URLs
+rss_feed_url = 'https://feeds.feedburner.com/crunchyroll/rss/anime'
+webhook_url = 'USE YOUR WEBHOOK'
+
+# Run the asynchronous function
+asyncio.run(post_to_guilded(rss_feed_url, webhook_url))
